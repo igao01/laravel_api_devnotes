22901440726a89ef93f9d7ab6e499f81f184f121
Igor Andrade <igorfernandodeandrade@gmail.com>
2021-03-22 18:56:08 -0300

    Função criar nova anotação finalizada

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Note;
 
@@ -23,7 +24,7 @@ class NoteController extends Controller
     }
 
     //EXIBE DETALHES DE UMA ANOTACAO
-    public function read ($id) {
+    public function read($id) {
         $array = ['error'=>''];
 
         $note = Note::find($id);
@@ -32,6 +33,32 @@ class NoteController extends Controller
             $array['data'] = $note;
         } else {
             $array['error'] = "A anotação não existe no banco de dados";
+        }
+        return $array;
+    }
+
+    //CRIA UMA NOVA ANOTAÇÃO
+    public function create(Request $request) {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        if(!$validator->fails()) {
+            $title = $request->input('title');
+            $body = $request->input('body');
+
+            $newNote = new Note();
+            $newNote->title = $title;
+            $newNote->body = $body;
+            $newNote->save();
+            $array['created'] = true;
+            
+        } else {
+            $array['created'] = false;
+            $array['error'] = "Não foi possível salvar sua anotação";
         }
         return $array;
     }
